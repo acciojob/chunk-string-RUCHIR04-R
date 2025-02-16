f34ae6c3c9d35147b12b157727b9a08d8bdbed4e
RUCHIR04-R <ruchirgawture@gmail.com>
2025-02-16 08:08:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function stringChop(str, size) {
   // your code here
+	if(str === null || size <= 0)
+	{
+		return[];
+	}
+	let chunk = [];
+	for(let i = 0;i < str.length; i += size){
+		chunk.push(str.slice(i, i + size));
+	}
+	return chunks;
 }
 
 // Do not change the code below
